cd7fad5219a55ba74284b45ccd82a349e3ea3ee4
Paliddo86 <L.pallante86@gmail.com>
2026-07-23 11:37:42 +0200

    Bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.paliddo.pdmxcontroller"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

6808fde93e50be2d5b46dde15b1caafb384f1544
Paliddo86 <L.pallante86@gmail.com>
2026-07-23 13:02:27 +0200

    Fix release permissions and bump version to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.paliddo.pdmxcontroller"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.3.1"
+        versionCode = 5
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
